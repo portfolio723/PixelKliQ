2a29f8d740ffa48c940980e8f16d74bc54d7cf47
Sandeep M <723msfree@gmail.com>
2025-07-16 12:19:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -61,7 +61,7 @@ const socials = [
   },
   {
     name: "Phone",
-    image: "https://img.icons8.com/color/96/phone.png",
+    image: "https://img.icons8.com/?size=100&id=a8unpNrefMCC&format=png&color=000000",
     dataAiHint: "phone icon",
     href: "tel:+911234567890",
   },
