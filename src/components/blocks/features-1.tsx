68b13e68a33d72adaea241967f66152b90b4c982
Sandeep M <723msfree@gmail.com>
2025-07-17 07:29:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardDecorator>
    
    R

--- src/components/blocks/features-1.tsx
+++ src/components/blocks/features-1.tsx
@@ -57,7 +57,7 @@ export function Features() {
 
 const CardDecorator = ({ children }: { children: ReactNode }) => (
     <div aria-hidden className="relative mx-auto size-36 [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]">
-        <div className="absolute inset-0 [--border:hsl(var(--border))] bg-[linear-gradient(to_right,var(--border)_1px,transparent_1px),linear-gradient(to_bottom,var(--border)_1px,transparent_1px)] bg-[size:24px_24px] opacity-10"/>
+        <div className="absolute inset-0 [--border:black] dark:[--border:white] bg-[linear-gradient(to_right,var(--border)_1px,transparent_1px),linear-gradient(to_bottom,var(--border)_1px,transparent_1px)] bg-[size:24px_24px] opacity-10"/>
         <div className="bg-background absolute inset-0 m-auto flex size-12 items-center justify-center border-t border-l">{children}</div>
     </div>
 )
