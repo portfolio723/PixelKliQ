246980834413988985b8eebfbd98704ddc45d8c8
Sandeep M <723msfree@gmail.com>
2025-07-17 19:53:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Features4>
    
    with

--- src/components/blocks/features-4.tsx
+++ src/components/blocks/features-4.tsx
@@ -4,11 +4,6 @@ export function Features4() {
     return (
         <section className="py-12 md:py-20">
             <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
-                <div className="relative z-10 mx-auto max-w-xl space-y-6 text-center md:space-y-12">
-                    <h2 className="text-balance text-4xl font-medium lg:text-5xl">The foundation for creative teams management</h2>
-                    <p className="text-muted-foreground">PixelKliQ is evolving to be more than just the models. It supports an entire ecosystem of APIs and platforms helping developers and businesses innovate.</p>
-                </div>
-
                 <div className="relative mx-auto grid max-w-2xl lg:max-w-4xl divide-x divide-y border *:p-12 sm:grid-cols-2 lg:grid-cols-3">
                     <div className="space-y-3">
                         <div className="flex items-center gap-2">
