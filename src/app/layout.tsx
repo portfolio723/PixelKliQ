7590f9bb9cb2c92956a4adfc2fa02543d1a04fef
Sandeep M <723msfree@gmail.com>
2025-07-19 10:31:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,7 @@ const fontCode = Source_Code_Pro({
 export const metadata: Metadata = {
   title: 'PixelKliQ',
   description: 'PixelKliQ offers cutting-edge digital solutions in Hyderabad, from creative design to tech development. Partner with us to elevate your brand.',
-  icons: {
-    icon: 'https://miro.medium.com/v2/resize:fit:356/format:webp/1*k-cMgo-BIeSv46lXogrEPQ.png',
-  },
+  icons: null,
 };
 
 export default function RootLayout({
@@ -33,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://miro.medium.com/v2/resize:fit:356/format:webp/1*k-cMgo-BIeSv46lXogrEPQ.png" />
       </head>
       <body className={cn("font-body antialiased", fontBody.variable, fontCode.variable)}>
         <Providers>
